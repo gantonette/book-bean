042f79a90f7fa22a86bb4d0443d53216bae0fad9
Gayle Wootton <gayle9281@gmail.com>
2023-02-12 16:29:58 +1300

    timeline changes

--- src/pages/TimelinePage.vue
+++ src/pages/TimelinePage.vue
@@ -1,56 +1,100 @@
 <template>
-  <div class="q-px-lg q-pb-md">
-    <q-timeline :layout="layout" color="secondary">
-      <q-timeline-entry heading>
-        Timeline heading
-        <br>
-        ({{$q.screen.lt.sm ? 'Dense' : ($q.screen.lt.md ? 'Comfortable' : 'Loose')}} layout)
-      </q-timeline-entry>
+  <div class="q-pa-lg">
 
-      <q-timeline-entry
-        title="Book Name"
-        subtitle="Publication Date"
-        side="left"
-      >
-        <div>
-          Description of the book and its content.
-        </div>
-      </q-timeline-entry>
+    <q-timeline :layout="layout" :side="side" color="secondary">
+      <q-timeline-entry heading>Book Bean</q-timeline-entry>
 
+      <template v-for="book in bookRepoList" :key="book.id">
       <q-timeline-entry
-        title="Another Book Name"
-        subtitle="Publication Date"
+        :title="book.name"
+        :subtitle="book.author"
         side="right"
-        icon="delete"
+        size="large"
       >
-        <div>
-          Description of the book and its content.
+        <div class="timeline-entry-content">
+          <!-- <q-img class="timeline-img" src='../assets/images/bookcover.png' /> -->
+          {{book.description}}
         </div>
       </q-timeline-entry>
+    </template>
 
-      <!-- Repeat the above structure for as many books as needed -->
     </q-timeline>
+
+    <q-card class="flex flex-center bg-light-green-3" style="width: 50%">
+      <div class="col-12">
+        <q-input color="green-8" v-model="booksToRead" label="book" />
+        <q-input color="green-8" v-model="author" label="author"/>
+        <q-input color="green-8" v-model="description" label="description">
+          <template v-slot:append>
+            <q-btn round color="green-3" icon="local_florist" :ripple="{ center: true }" @click="addItem"/>
+          </template>
+        </q-input>
+      </div>
+    </q-card>
   </div>
 </template>
 
 
 <script>
-
 import { defineComponent, ref } from "vue";
 import { db } from './firebase';
 import { getFirestore, collection, onSnapshot, doc, deleteDoc, addDoc} from "firebase/firestore";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
-const bookRepo = collection(db, 'bookRepo')
 
-export default defineComponent ({
-  name: 'TimelinePage',
-  setup() {
-    const bookRepoList = ref([])
-    onSnapshot(bookRepo, (snapshot) => {
-      bookRepoList.value = snapshot.docs.map(doc => ({ ...doc.data(), id: doc.id }))
-    })
-    return {
-      bookRepoList
+const auth = getAuth();
+const user = ref(auth.currentUser);
+let uid;
+
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    uid = user.uid;
+    console.log('this is the user: ' + uid)
+    user.value = uid;
+  } else {
+    console.log('no user');
+  }
+});
+
+const bookRepo = ref();
+
+export default defineComponent({
+name: "TimelinePage",
+
+setup() {
+
+  const bookRepoList = ref([])
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      const uid = user.uid;
+      bookRepo.value = collection(db, 'users', uid, 'bookRepo');
+      onSnapshot(bookRepo.value, (snapshot) => {
+        bookRepoList.value = snapshot.docs.map(doc => ({ ...doc.data(), id: doc.id }))
+        console.log(bookRepoList.value)
+      });
+    }
+  });
+
+  return {
+    bookRepoList,
+    layout: ref('loose'),
+    side: ref('right')
+  }
+},
+
+  methods: {
+    addItem() {
+      addDoc(bookRepo.value, { name: this.booksToRead, author: this.author, description: this.description }).then(() => {
+        this.booksToRead = ''
+        this.author = ''
+        this.description = ''
+      })
+    },
+    async removeBookRepo(id) {
+      if(id) {
+        await deleteDoc(doc(db,'users', uid, 'bookRepo', `${id}`));
+
+      }
     }
   }
 })
